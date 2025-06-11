5bcca87819b0c9dc99d8272f9423a4c0fcac5b2f
localworkarea <localworkarea@gmail.com>
2025-06-11 21:03:21 +0300

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,34 @@
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+
+   // ==   VIDEO YOUTUBE ON CLICK BUTTON ==================================================
+    const videoYoutubeButtons = document.querySelectorAll('.video-youtube__button');
+    videoYoutubeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const youTubeCode = this.getAttribute('data-youtube');
+            let autoplay = true; // Автоплей разрешено (true) или нет (false)
+    
+            let urlVideo = `https://www.youtube.com/embed/${youTubeCode}?rel=0&showinfo=0`;
+    
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('allowfullscreen', '');
+    
+            if (autoplay) {
+                urlVideo += '&autoplay=1';
+                iframe.setAttribute('allow', 'autoplay; encrypted-media');
+            }
+    
+            iframe.setAttribute('src', urlVideo);
+    
+            const body = this.closest('.video-youtube__body');
+            const bodyWrap = this.closest('.video-youtube');
+            body.innerHTML = '';
+            body.appendChild(iframe);
+            body.classList.add('video-added');
+            bodyWrap.classList.add('video-play');
+        });
+    });
+    // =====================================================================================
+  
